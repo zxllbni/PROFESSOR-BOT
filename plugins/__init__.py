1f6f2911ebc16d3e788dfdf60a910c5e0127433e
zxllbni <webhostooo70@gmail.com>
2024-05-13 08:58:40 +0530

    Add files via upload

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -10,7 +10,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response(text="ᴍᴀᴅᴇ ʙʏ: Mᴋɴ Bᴏᴛs™")
+    return web.json_response(text="ᴍᴀᴅᴇ ʙʏ: Noob Bots™")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
